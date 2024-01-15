1a86577912d0c0bb40c4c874dbdf8f4a612690ce
vishal patel <vishallovevap@gmail.com>
2024-01-15 22:32:22 +0530

    base url final

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,6 +1,6 @@
 // export const base_url = "http://localhost:5000/api/"; // Replace with your API's base URL
-export const base_url = "https://ecommerce-backend-seven-pi.vercel.app/api/"; // Replace with your API's base URL
 
+export const base_url = "https://ecommerce-backend-seven-pi.vercel.app/api/";
 const getTokenFromLocalStorage = localStorage.getItem("customer")
   ? JSON.parse(localStorage.getItem("customer"))
   : null;
