490ee1bb6540916959f8fd01d911907eaca571f4
vishal patel <vishallovevap@gmail.com>
2024-01-15 22:14:59 +0530

    base url change

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,4 +1,5 @@
-export const base_url = "http://localhost:5000/api/"; // Replace with your API's base URL
+// export const base_url = "http://localhost:5000/api/"; // Replace with your API's base URL
+export const base_url = "https://ecommerce-backend-seven-pi.vercel.app/api/"; // Replace with your API's base URL
 
 const getTokenFromLocalStorage = localStorage.getItem("customer")
   ? JSON.parse(localStorage.getItem("customer"))
